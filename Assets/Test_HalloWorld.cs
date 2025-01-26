6d0c13288875f109177cb45a4ba2702717b63ca7
Kirby-rinrin <rinrin.kawady@gmail.com>
2025-01-26 18:06:34 +0900

    ハローワールド！！

---
+++ Assets/Test_HalloWorld.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_HalloWorld : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hallo World!");
+    }
+    void Update()
+    {
+        
+    }
+}
