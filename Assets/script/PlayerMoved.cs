694bfcf2fc67626cdb06a55974f12afe0ddc8d22
Kirby-rinrin <rinrin.kawady@gmail.com>
2025-02-13 20:40:57 +0900

    クリエイティブモードを作っておいたよ

--- Assets/script/PlayerMoved.cs
+++ Assets/script/PlayerMoved.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMoved : MonoBehaviour
 {
@@ -10,30 +11,86 @@ public class PlayerMoved : MonoBehaviour
     private int MoveSpeed;
     [SerializeField]
     private float RotateSpeed;
+    [SerializeField]
+    private float jumpPower;
     private Vector3 startPosi;
-
+    private bool grounded;
+    public static bool creativity;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         startPosi = player.position;
+        creativity = false;
+        Debug.Log("not creativity!");
     }
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))    //移動
+        if (Input.GetKeyDown(KeyCode.C))
         {
-            transform.position += transform.TransformDirection(Vector3.forward * MoveSpeed * Time.deltaTime);//(grounded)?==if(grouded) // dashSpeed : jumpSpeed  == true : false
+            ChangeCreativity();
         }
-        if (Input.GetKey(KeyCode.S))
+        if (!creativity)
         {
-            transform.position += transform.TransformDirection(Vector3.back * MoveSpeed * Time.deltaTime);
+            rb.useGravity = true;
+            if (Input.GetKey(KeyCode.W))    //移動
+            {
+                transform.position += transform.TransformDirection(Vector3.forward * MoveSpeed * Time.deltaTime);//(grounded)?==if(grouded) // dashSpeed : jumpSpeed  == true : false
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position += transform.TransformDirection(Vector3.back * MoveSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.position += transform.TransformDirection(Vector3.left * MoveSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += transform.TransformDirection(Vector3.right * MoveSpeed * Time.deltaTime);
+            }
+            if (Input.GetKeyDown(KeyCode.Space))    //ジャンプ
+            {
+                if (grounded)
+                {
+                    rb.AddForce(Vector3.up * jumpPower);
+                }
+            }
         }
-        if (Input.GetKey(KeyCode.A))
+        else
         {
-            transform.position += transform.TransformDirection(Vector3.left * MoveSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += transform.TransformDirection(Vector3.right * MoveSpeed * Time.deltaTime);
+            rb.useGravity = false;
+            if (Input.GetKey(KeyCode.W))    //移動
+            {
+                transform.position += transform.TransformDirection(Vector3.forward * MoveSpeed * 10 * Time.deltaTime);//(grounded)?==if(grouded) // dashSpeed : jumpSpeed  == true : false
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position += transform.TransformDirection(Vector3.back * MoveSpeed * 10 * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.position += transform.TransformDirection(Vector3.left * MoveSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += transform.TransformDirection(Vector3.right * MoveSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.Space))    //ジャンプ
+            {
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    rb.AddForce(Vector3.down * MoveSpeed * 5);
+                }
+                else
+                {
+                    rb.AddForce(Vector3.up * MoveSpeed * 5);
+                }
+            }
+            else
+            {
+                rb.velocity = Vector3.zero;
+            }
+
         }
         if (Input.GetKey(KeyCode.LeftArrow))//方向転換
         {
@@ -49,6 +106,40 @@ public class PlayerMoved : MonoBehaviour
             transform.position = startPosi;
             rb.velocity = Vector3.zero;
             rb.angularVelocity = Vector3.zero;
+        }
+    }
+    void OnCollisionEnter(Collision collision)//○○に当たったとき
+    {
+        if (collision.gameObject.tag == "Goal")
+        {
+            SceneManager.LoadScene(collision.gameObject.GetComponent<Teleporter>().Warpworld);
+            Debug.Log("To be continued...");
+        }
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = false;
+        }
+    }
+    void ChangeCreativity()
+    {
+        creativity = !creativity;
+        Debug.Log("change creativity!");
+        if (creativity)
+        {
+            Debug.Log("Now it's creativity!");
+            //Angelcircle.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Now it's not creativity!");
+            //Angelcircle.SetActive(false);
         }
     }
 }
---
+++ Assets/script/Teleporter.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField]
+    public string Warpworld = "TestMazeGame";
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+}
